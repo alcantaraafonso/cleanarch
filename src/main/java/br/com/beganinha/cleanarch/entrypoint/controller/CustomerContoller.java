8a7ada39fedde3415ac48b8d47b2ec5b8e177c57
Afonso Alcantara <alcantara.afonso@gmail.com>
2023-03-19 18:09:05 -0300

    Adding the entrypoints to FindCustomerUseCase

--- src/main/java/br/com/beganinha/cleanarch/entrypoint/controller/CustomerContoller.java
+++ src/main/java/br/com/beganinha/cleanarch/entrypoint/controller/CustomerContoller.java
@@ -1,16 +1,15 @@
 package br.com.beganinha.cleanarch.entrypoint.controller;
 
 import br.com.beganinha.cleanarch.core.domain.Customer;
+import br.com.beganinha.cleanarch.core.usacase.FindCustomerUseCase;
 import br.com.beganinha.cleanarch.core.usacase.InsertCustomerUseCase;
 import br.com.beganinha.cleanarch.entrypoint.controller.mapper.CustomerMapper;
 import br.com.beganinha.cleanarch.entrypoint.controller.request.CustomerRequest;
+import br.com.beganinha.cleanarch.entrypoint.controller.response.CustomerResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -22,6 +21,9 @@ public class CustomerContoller {
     private InsertCustomerUseCase insertCustomerUseCase;
 
     @Autowired
+    private FindCustomerUseCase findCustomerUseCase;
+
+    @Autowired
     private CustomerMapper customerMapper;
 
     @PostMapping
@@ -30,5 +32,13 @@ public class CustomerContoller {
         insertCustomerUseCase.insert(customer, customerRequest.getZipCode());
 
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CustomerResponse> getById(@PathVariable("id") String id) {
+        Customer customer = findCustomerUseCase.findById(id);
+        CustomerResponse customerResponse = customerMapper.toCustomerResponse(customer);
+
+        return ResponseEntity.ok().body(customerResponse);
     }
 }
