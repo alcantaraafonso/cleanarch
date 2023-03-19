d904802bda2dc129532ce9b338f5f689f148d826
Afonso Alcantara <alcantara.afonso@gmail.com>
2023-03-19 18:01:14 -0300

    Adding class configuration to FindCustomerUserCase

---
+++ src/main/java/br/com/beganinha/cleanarch/config/FindCustomerConfig.java
@@ -0,0 +1,17 @@
+package br.com.beganinha.cleanarch.config;
+
+import br.com.beganinha.cleanarch.core.usacase.impl.FindCustomerUseCaseImpl;
+import br.com.beganinha.cleanarch.dataprovider.FindCustomerImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FindCustomerConfig {
+
+    @Bean
+    public FindCustomerUseCaseImpl findCustomerUseCase(
+            FindCustomerImpl findCustomer
+    ) {
+        return new FindCustomerUseCaseImpl(findCustomer);
+    }
+}
